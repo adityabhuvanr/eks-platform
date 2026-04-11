86e95dbdabcb011f6379c420a7858b4790be1e8f
adityabhuvanr <adityabhuvanraj36@gmail.com>
2026-04-11 17:04:49 +0530

    Add root variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,57 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+  default = "1.34"
+}
+
+variable "node_instance_types" {
+  type = list(string)
+}
+
+variable "desired_size" {
+  type = number
+}
+
+variable "min_size" {
+  type = number
+}
+
+variable "max_size" {
+  type = number
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
